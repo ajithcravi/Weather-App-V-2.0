1d2f4ddf3fb19a0bb7a7b6c1d8f4e32aef66dbf0
ajithcravi <craviajith@gmail.com>
2019-11-05 09:46:53 +0530

    css file added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
   res.render('index')
 });
