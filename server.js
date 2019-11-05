c752d517e355dc6e9900369dbc03dbfd69be138a
ajithcravi <craviajith@gmail.com>
2019-11-05 10:00:09 +0530

    post route done

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.post('/', (req, res) => {
+  res.render('index');
+  console.log(req.body.city);
 });
 
 app.listen(3000, () => {
